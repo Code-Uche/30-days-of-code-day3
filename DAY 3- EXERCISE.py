cdb4f05ea92d052784d58b191d90c782c1394552
Code-Uche <122784703+Code-Uche@users.noreply.github.com>
2023-12-20 20:40:50 +0000

    Day 3 exercise

---
+++ DAY 3- EXERCISE.py
@@ -0,0 +1,17 @@
+#Day 3 exercises. Number 1
+greeting = "Hello, world"
+print(f"{greeting}!") #I used f string to solve this exercise
+
+#Number 2
+name = input("Please enter your name: ").strip().title()
+print(f"Hello, {name}!")
+
+#Number 3
+age = str(29) #I used str() to convert the number to string
+print("I am " + age)
+
+#Number 4
+title = "Joker"
+director = "Todd Phillips"
+release_year = str(2019) 
+print(f"{title} {release_year}, directed by {director}")
